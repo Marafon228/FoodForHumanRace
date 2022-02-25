2c5e2c3cf4093a0c434e16b5caa5c2871d0e6c0e
Aaaa <d>
2022-02-25 11:37:52 +0300

    Add project files.

---
+++ ObservableObject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+public class ObservableObject : INotifyPropertyChanged
+{
+	public event PropertyChangedEventHandler PropertyChanged;
+	public void OnPropertyChanged([CallerMemberName] string callerMemberName = "") =>
+		PropertyChanged?.Invoke(this. new PropertyChangedEventArgs(callerMemberName));
+}
