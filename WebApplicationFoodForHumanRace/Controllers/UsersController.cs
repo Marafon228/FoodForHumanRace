8014b06d7c46c8571477d2b810db84e54a6b7279
Сергей Денисов <denisoff.vaniok2016@mail.ru>
2022-11-11 14:28:02 +0300

    add EnableCors

--- WebApplicationFoodForHumanRace/Controllers/UsersController.cs
+++ WebApplicationFoodForHumanRace/Controllers/UsersController.cs
@@ -7,12 +7,14 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 using System.Web.Http.Description;
 using WebApplicationFoodForHumanRace.Models;
 using WebApplicationFoodForHumanRace.Models.Json;
 
 namespace WebApplicationFoodForHumanRace.Controllers
 {
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class UsersController : ApiController
     {
         private ADO db = new ADO();
